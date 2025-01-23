dfed7c55e6a09be0b45544c0b7827b72c1d97f48
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-01-23 21:46:13 +0000

    finishing the practice01

--- practice/practice01/main.cpp
+++ practice/practice01/main.cpp
@@ -1,18 +1,69 @@
 #include <iostream>//no ai used
 #include <vector>
-#include <limits>
+#include <limits>//for getInt()
 
 //forward declarations:
 int getInt(const std::string& prompt);
+void printVector(const std::vector<int>& vec);
+std::vector<int>& doubleVector(std::vector<int>& vec);
+int getSum(std::vector<int>& vec);
+void printMultiples(const std::vector<int>& vec, int multiple);
 
-//menu
+
 int main(){
 
+    std::vector<int> vec {};
+
     while(true){
+        //the menu
         std::cout << "\n1. Add item to vector\n2. Print the vector\n3. Double the vector"
         << "\n4. Find the sum\n5. Print multiples of a user-defined value\n6. Exit\n";
 
-        std::cout << "Enter a number 1-6 (inclusive): ";
+        int num {getInt("Enter a number 1-6 (inclusive): ")};
+
+        if(num == 1){
+            std::cout << "You chose to add item to vector\n";
+
+            int addedNum {getInt("Enter the number you want to add to the vector: ")};
+
+            vec.push_back(addedNum);
+            std::cout << "Number added\n";
+        }
+
+        else if(num == 2){
+            std::cout << "You chose to print the vector\n";
+            printVector(vec);
+        }
+
+        else if(num == 3){
+            std::cout << "You chose to double the vector\n";
+            doubleVector(vec);
+            std::cout << "The numbers in the vector have been doubled\n";
+        }
+
+        else if(num == 4){
+            std::cout << "You chose to find the sum\n";
+
+            std::cout << "The sum is " << getSum(vec) << '\n';
+        }
+
+        else if(num == 5){
+            std::cout << "You chose to print multiples of a user defined value\n";
+
+            int multiple {getInt("Enter a number: ")};
+
+            printMultiples(vec, multiple);
+        }
+
+        else if(num == 6){
+            std::cout << "You chose to exit\n";
+            std::cout << "Goodbye.\n";
+            break;//exits loop
+        }
+
+        else{
+            std::cout << "That was not one of the numbers listed. Please try again\n";
+        }
     }
 
     return 0;
@@ -21,22 +72,20 @@ int main(){
 
 //actual functions
 int getInt(const std::string& prompt){
+
     int input {};
 
     while (true){
         std::cout << prompt;
         std::cin >> input;
     
-        if(std::cin.fail() || input < 0){
+        if(std::cin.fail()){
             std::cin.clear(); //clears the error
         
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             //discards invalid input
         
-             std::cout << "Inproper input. please try again. "
-            << "Text should reappear for ship inputs except for mission inputs." 
-            << " For that just type below for inputs and hit enter. "
-            << "DO IT RIGHT THIS TIME.\n";
+             std::cout << "Inproper input. please try again.\n";
         }
         else{
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -45,4 +94,38 @@ int getInt(const std::string& prompt){
             break;//exits loop
         }
     }
+}
+
+void printVector(const std::vector<int>& vec){//don't want to modify vector so const
+    for(int num : vec){
+        std::cout << num << ' ';
+    }
+}
+
+std::vector<int>& doubleVector(std::vector<int>& vec){//uses pass by ref to actually modify the values
+    for(int i{0}; i < vec.size(); i++){
+        vec.at(i) *= 2;
+    }
+
+    return vec;
+}
+
+int getSum(std::vector<int>& vec){
+    int sum{};
+    for(int i{0}; i < vec.size(); i++){
+        sum += vec.at(i);
+    }
+
+    return sum;
+}
+
+void printMultiples(const std::vector<int>& vec, int multiple){
+    std::cout << "The multiples of " << multiple << " are as follows: ";
+    for(int i{0}; i < vec.size(); i++){
+        if(vec.at(i) % multiple == 0){
+            std::cout << vec.at(i) << ' ';
+        }
+    }
+
+    std::cout << "\nThat's all folks!\n";
 }
