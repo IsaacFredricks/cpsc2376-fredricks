0d8ca18f60f3f28820da32979383e0153a96bf2f
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-01-28 14:48:42 +0000

    setting up files for project 01 and practice02

---
+++ practice/practice02/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>//no ai used
+#include <vector>
+//may have to add something for random numbers or file stream
+
+int main(){
+    //insert code here
+
+    return 0;
+}
---
+++ projects/project01/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//no ai used.
+#include <vector>
+#include <string>//for string functions
+#include <fstream>//for file reading and writing. ifstream is for reading files, fstream is for writing to them
+
+int main(){
+    //not done. 
+
+    return 0;
+}
