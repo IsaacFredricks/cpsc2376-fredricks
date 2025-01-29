05cb173921bb5aab16fb36c5b4ff71fa380b83d2
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-01-29 17:58:09 -0600

    project 01 file setup on vs

--- projects/project01/main.cpp
+++ projects/project01/main.cpp
@@ -1,10 +1,20 @@
-#include <iostream>//no ai used.
-#include <vector>
-#include <string>//for string functions
-#include <fstream>//for file reading and writing. ifstream is for reading files, fstream is for writing to them
+// project01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 
-int main(){
-    //not done. 
+#include <iostream>
 
-    return 0;
+int main()
+{
+    std::cout << "Hello World!\n";
 }
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
