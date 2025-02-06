0ce112553b014498ac59520edb3bb98db38c5e2a
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-02-06 15:43:35 -0600

    set up practice03

---
+++ practice/practice03/fixes.md
@@ -0,0 +1,39 @@
+pre bug fix results:
+
+*TestSumRange1 failed: Expected value= 21. Actual= 15
+
+*TestSumRange2 failed: Expected value= 0. Actual= -5
+
+*TestSumRange3 failed: Expected value= -21. Actual= -20
+
+*TestContainsNegative1 failed: Expected value= false. Actual= vector out of range
+
+*TestContainsNegative2 failed: Expected value= true. Actual= error. vector out of range
+
+*TestContainsNegative3 passed but for wrong reason: Expected value= true. Actual= true.
+
+*TestContainsNegative4 failed: Expected value= false. Actual= vector out of range
+
+*TestFindMax1 failed: Expected value= 1. Actual= vector out of range
+
+*TestFindMax2 passed: Expected value = 0. Actual= 0
+
+*TestFindMax3 failed: Expected value = 33. Actual= vector out of range
+
+bugs:
+
+*line 9: <= instead of <
+
+*line 10:
+
+*
+
+*
+
+*
+
+
+
+
+
+
---
+++ tests/practice03_test/practice03_test.cpp
@@ -0,0 +1,84 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../practice/practice03/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace practice03test
+{
+	TEST_CLASS(practice03test)
+	{
+	public:
+		
+		TEST_METHOD(TestSumRange1)
+		{
+			Assert::AreEqual(21, MathUtils::sumRange(0, 6));
+
+		}
+
+		TEST_METHOD(TestSumRange2)
+		{
+			Assert::AreEqual(0, MathUtils::sumRange(-5, 5));
+		}
+
+		TEST_METHOD(TestSumRange3)
+		{
+			Assert::AreEqual(-21, MathUtils::sumRange(-6, 0));
+		}
+
+		TEST_METHOD(TestContainsNegative1)
+		{
+			std::vector<int> allPositive{ 1, 2, 3, 4 };
+
+			Assert::IsFalse(MathUtils::containsNegative(allPositive));
+		}
+
+		TEST_METHOD(TestContainsNegative2)
+		{
+			std::vector<int> allNegative{ -44, -10, -11, -22 };
+
+			Assert::IsTrue(MathUtils::containsNegative(allNegative));
+			
+		}
+
+		TEST_METHOD(TestContainsNegative3)
+		{
+			std::vector<int> mixedVec{ 1, -1, 2, -3 };
+
+			Assert::IsTrue(MathUtils::containsNegative(mixedVec));
+		}
+
+		TEST_METHOD(TestContainsNegative4)
+		{
+			std::vector<int> emptyVec{};
+
+			Assert::IsFalse(MathUtils::containsNegative(emptyVec));
+		}
+		
+		TEST_METHOD(TestFindMax1)
+		{
+			std::vector<int> sameNums{1, 1, 1, 1};
+
+			Assert::AreEqual(1, MathUtils::findMax(sameNums));
+		}
+
+		TEST_METHOD(TestFindMax2)
+		{
+			std::vector<int> emptyVector{};
+
+			Assert::AreEqual(0, MathUtils::findMax(emptyVector));
+		}
+
+		TEST_METHOD(TestFindMax3)
+		{
+			std::vector<int> mixedNums{-11, 33, 1, 4};
+
+			Assert::AreEqual(0, MathUtils::findMax(mixedNums));
+		}
+
+
+
+
+
+	};
+}
