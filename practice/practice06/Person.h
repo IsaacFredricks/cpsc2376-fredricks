4eed1bec8e2b4565fecf9aeab0f4b81480451c13
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-02-26 17:48:05 -0600

    messed around with classes and inheritance

--- practice/practice06/Person.h
+++ practice/practice06/Person.h
@@ -1,10 +1,21 @@
 #pragma once
+#include <string>
+#include <iostream>
 
 class Person {
-	int age{};
+	int age;
+	std::string name;
 
 public:
-	int getAge();
+	Person(std::string name, int age=1);
+
+	int getAge() const;
 
 	void setAge(int age);
+
+	std::string getName() const;
+
+	//operator overload
+	friend Person operator+(Person& p1, Person& p2);//not technically in the class, but can access class instance variables
+	friend std::ostream& operator<<(std::ostream& os, Person& p2);//not technically in the class, but can access class instance variables
 };
--- practice/practice06/main.cpp
+++ practice/practice06/main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>//no ai used
 #include "Person.h"
+#include <string>
 
+class SuperPerson : Person{
+	int hp;
+public:
+	SuperPerson(std::string name, int age, int hp = 100) : Person(name, age) , hp{hp} {}//has to call derived class person
+
+	void talk() {
+		std::cout << "hi\n";
+	}
+};
 int main() {
-	Person p;
+	Person p("bob", 3);
+	Person j("jill");//works because default value in header
 	//std::cout << p.age;//can't do. it's private
 
 	std::cout << p.getAge() << '\n';
 
 	p.setAge(5);
+	
+	p = j + p;
 
 	std::cout << p.getAge() << '\n';
+	std::cout << p.getName() << '\n';
+	std::cout << p << '\n';
+	
+	SuperPerson sp("james", 5, 16);
+
+	sp.talk();
 	return 0;
 }
