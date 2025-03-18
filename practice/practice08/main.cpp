74bc1fceb67885d2076444679a5ed4e988ebf278
Isaac Fredricks <ihenryfredricks@gmail.com>
2025-03-18 08:55:31 -0500

    for some reason github isn't working JUST WORK

--- practice/practice08/main.cpp
+++ practice/practice08/main.cpp
@@ -1,142 +1,65 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <cmath>
 #include <vector>
-#include <cassert>
+#include <algorithm>
+#include <string>
+#include <sstream>
 
-class Shape {//interface class. only virtual functions
-public:
-    virtual double area() = 0;
+/*
+Ask the user to enter a list of space-separated numbers (e.g., 5 2 9 1 7).
+Store the numbers in a std::vector<int>.
+Use std::sort with a lambda function to implement the following custom sorting rule:
+All odd numbers are considered larger than all even numbers.
+Among odd numbers, sort in descending order.
+Among even numbers, sort in descending order.*/
 
-    static Shape* build(std::string s);
-};
+int main() {
+	std::cout << "Enter a space separated list of numbers: ";
+	std::string line{};
 
-class Circle : public Shape{
+	std::getline(std::cin, line);
+	std::vector<int> nums{};
 
-private:
-    double r;
+	std::istringstream ssin(line);
+	while (true) {
+		//don't know how to stop while loopso psuedo code:
+		//gets number from string
+		//if it fails to get number, it exits the loop
+		int num{};
+		ssin >> num;
 
-public:
-    Circle(double r) : r{ r } {}
+		//example code: if(ssin.fail()) break;
+		nums.push_back(num);
+	}
 
-    double area() override{//only override things you want to be polymorphic
-        return 3.14159 * r * r;//M_PI didn't work for me
-    }
+	std::cout << "input: ";
+	std::for_each(nums.begin(), nums.end(), [](int n) {std::cout << n << ' ';});
 
-    static Shape* build(std::string s) {
-        std::istringstream ssin(s);
-        std::string type;
-        ssin >> type;
-        assert(type == "CIRCLE");
-        std::cout << "building circle\n";
-        double r;
-        ssin >> r;
-        return new Circle(r);
-    }
-};
+	std::cout << '\n';
 
-class Rectangle : public Shape {
-private:
-    double w;
-    double h;
+	std::sort(nums.begin(), nums.end(), [](int a, int b) {
+		if (a % 2 == 0 && b % 2 == 0) {
+			return a < b;
+		}
 
-public:
-    Rectangle(double w, double h) : w{ w }, h { h } {}
+		else if (a % 2 > 0 && b % 2 > 0) {
+			return a > b;
+		}
 
-    double area() override {//only override things you want to be polymorphic
-        return w * h;//M_PI didn't work for me
-    }
+		else if (a % 2 > 0 && b % 2 == 0 && a > b || a < b) {
+			return a > b;
+		}
 
-    static Shape* build(std::string s) {
-        std::istringstream ssin(s);
-        std::string type;
-        ssin >> type;
-        assert(type == "RECTANGLE");
-        std::cout << "building circle\n";
-        double w;
-        ssin >> w;
+		else if (a % 2 == 0 && b % 2 > 0 && a > b || a < b) {
+			return a < b;
+		}
 
-        double h;
-        ssin >> h;
-        return new Circle(w);
-    }
-};
+		});
 
-class Square : public Shape {
-private:
-    double l;
+	std::cout << "output: ";
+	std::for_each(nums.begin(), nums.end(), [](int n) {std::cout << n << ' ';});
 
-public:
-    Square(double l) : l{ l } {}
+	std::cout << '\n';
 
-    double area() override {//only override things you want to be polymorphic
-        return l * l;//M_PI didn't work for me
-    }
 
-    static Shape* build(std::string s) {
-        std::istringstream ssin(s);
-        std::string type;
-        ssin >> type;
-        assert(type == "SQUARE");
-        std::cout << "building circle\n";
-        double l;
-        ssin >> l;
-        return new Square(l);
-    }
-};
-
-Shape* Shape::build(std::string s) {
-    //map with shapetypes as key and factor functions as values
-    if (s.at(0) == 'R') return Rectangle::build(s);
-    else if (s.at(0) == 'S') return Square::build(s);
-    else if (s.at(0) == 'C') return Circle::build(s);
-    else return nullptr;
-
-    return nullptr;
+	return 0;
 }
-
-int main()
-{
-    /*
-    Circle c(5);//can't make derived class without defining base class virtual functions
-    std::cout << c.area() << '\n';
-
-    Square s(5);
-
-    Rectangle r(5, 6);
-    std::cout << s.area() << '\n';
-    std::cout << r.area() << '\n';
-    */
-
-    //avoid pass by value with base-derive objects. does obj slicing
-
-    std::vector<Shape*> shapes;//use pointers instead
-
-    std::ifstream fin("shapes.txt");//reading file
-    
-
-    while (true) {
-        std::string line;
-
-        std::getline(fin, line);
-
-        if (line == "") break; 
-
-        std::cout << line << '\n';
-
-        shapes.push_back(Shape::build(line));
-    }
-
-    double area{ 0 };
-
-    for (auto* s : shapes) {
-        area += s->area();
-    }
-
-    std::cout << "total area is " << area << '\n';
-    std::cout << "vector size is " << shapes.size() << '\n';
-    return 0;
-}
-
